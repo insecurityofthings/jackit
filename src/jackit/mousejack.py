8e21f785e672f64374d6fdde53ebd1df7ab80638
phikshun <phikshun@users.noreply.github.com>
2017-06-10 18:56:52 -0700

    minor tweaks

--- src/jackit/mousejack.py
+++ src/jackit/mousejack.py
@@ -13,20 +13,18 @@ class MouseJack(object):
         self.channel_index = 0
         self.debug = debug
         self.devices = {}
+        self.ping = '\x0f\x0f\x0f\x0f'
         self.init_radio(disable_lna, reset)
 
     def _debug(self, text):
         if self.debug:
             print(text)
 
+    def hex_to_ints(self, data):
+        return [int(b, 16) for b in data.split(':')]
+
     def hexify(self, data):
         return ':'.join('{:02X}'.format(x) for x in data)
-
-    def unhexify_addr(self, val):
-        return self.unhexify(val)[::-1][:5]
-
-    def unhexify(self, val):
-        return val.replace(':', '').decode('hex')
 
     def serialize_payload(self, p):
         return str(bytearray(p))
@@ -36,7 +34,7 @@ class MouseJack(object):
 
     def init_radio(self, disable_lna, reset):
         if reset:
-            self._debug("Reseting PARadio USB Dongle")
+            self._debug("Resetting USB dongle")
             nrf24_reset.reset_radio(0)
         self.radio = nrf24.nrf24(0)
         if not disable_lna:
@@ -58,7 +56,7 @@ class MouseJack(object):
             self.devices[address]['count']     = 1
             self.devices[address]['timestamp'] = time.time()
             self.devices[address]['channels']  = [self.channels[self.channel_index]]
-            self.devices[address]['address']   = [int(b, 16) for b in address.split(':')]
+            self.devices[address]['address']   = self.hex_to_ints(address)
             self.devices[address]['device']    = self.get_hid(payload)
             self.devices[address]['payload']   = payload
 
@@ -78,30 +76,27 @@ class MouseJack(object):
             value = self.radio.receive_payload()
             if len(value) >= 5:
                 address, payload = value[0:5], value[5:]
-                a = self.hexify(address)
-                self._debug("ch: %02d addr: %s packet: %s" % (self.channels[self.channel_index], a, self.hexify(payload)))
-                self.add_device(a, payload)
+                self._debug("ch: %02d addr: %s packet: %s" % (self.channels[self.channel_index], self.hexify(address), self.hexify(payload)))
+                self.add_device(self.hexify(address), payload)
 
         # TODO: Need to catch RuntimeError in jackit
         return self.devices
 
-    def sniff(self, timeout, address):
-        addr_string = address[:]
-        address = [int(b, 16) for b in address.split(':')]
+    def sniff(self, timeout, addr_string):
+        address = self.hex_to_ints(addr_string)
         self.radio.enter_sniffer_mode(self.serialize_address(address))
         self.radio.set_channel(self.channels[self.channel_index])
         dwell_time = 0.1
-        ping = '0F:0F:0F:0F'.replace(':', '').decode('hex')
         last_ping = time.time()
         start_time = time.time()
 
         while time.time() - start_time < timeout:
             if len(self.channels) > 1 and time.time() - last_ping > dwell_time:
-                if not self.radio.transmit_payload(self.serialize_payload(ping), 1, 1):
+                if not self.radio.transmit_payload(self.ping, 1, 1):
                     success = False
                     for self.channel_index in range(len(self.channels)):
                         self.radio.set_channel(self.channels[self.channel_index])
-                        if self.radio.transmit_payload(self.serialize_payload(ping), 1, 1):
+                        if self.radio.transmit_payload(self.ping, 1, 1):
                             last_ping = time.time()
                             success = True
                             self._debug("Ping success on channel %d" % self.channels[self.channel_index])
@@ -126,11 +121,10 @@ class MouseJack(object):
         self.radio.enter_sniffer_mode(self.serialize_address(address))
 
     def find_channel(self, address):
-        ping = '0F:0F:0F:0F'.replace(':', '').decode('hex')
         self.radio.enter_sniffer_mode(self.serialize_address(address))
         for channel in range(2, 84):
             self.radio.set_channel(channel)
-            if self.radio.transmit_payload(self.serialize_payload(ping)):
+            if self.radio.transmit_payload(self.ping):
                 return channel
         return None
 
@@ -138,11 +132,8 @@ class MouseJack(object):
         self.current_channel = channel
         self.radio.set_channel(channel)
 
-    def transmit_hook(self, payload):
-        self._debug("Sending: " + self.hexify(payload))
-
     def transmit_payload(self, payload):
-        self.transmit_hook(payload)
+        self._debug("Sending: " + self.hexify(payload))
         return self.radio.transmit_payload(self.serialize_payload(payload))
 
     def get_hid(self, p):
