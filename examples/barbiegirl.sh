e9b6a62ec349493db2b0d3d672bc94a3329e8fa6
phikshun <phikshun@users.noreply.github.com>
2017-06-25 16:59:53 -0700

    Add new macOS troll

---
+++ examples/barbiegirl.sh
@@ -0,0 +1,2 @@
+# Inspired by: https://github.com/EmpireProject/Empire/blob/master/lib/modules/python/trollsploit/osx/thunderstruck.py
+osascript -e "set Volume 100" && open -a "Safari" -g -j 'https://youtu.be/ZyhrYis509A?t=13' && exit
